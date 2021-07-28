addfd21f15c222022c0fbaed51a81719f754c1db
IBNSINA <2002>
2021-07-28 13:09:50 +0600

    Add Facade

---
+++ src/main/java/com/example/spocktesting/util/facade/ClientFacade.java
@@ -0,0 +1,55 @@
+package com.example.spocktesting.util.facade;
+
+import com.example.spocktesting.dto.request.client.ClientCreateDtoRequest;
+import com.example.spocktesting.dto.request.client.ClientUpdateDtoRequest;
+import com.example.spocktesting.dto.response.client.ClientDtoResponse;
+import com.example.spocktesting.model.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientFacade {
+    public static ClientDtoResponse modelToDtoResponse(Client model) {
+        ClientDtoResponse dto = new ClientDtoResponse();
+        if (model.getId() != null)
+            dto.setId(model.getId());
+        if (model.getName() != null)
+            dto.setName(model.getName());
+        if (model.getSurname() != null)
+            dto.setSurname(model.getSurname());
+        if (model.getPhoneNumber() != null)
+            dto.setPhoneNumber(model.getPhoneNumber());
+        if (model.getDiscount() != null)
+            dto.setDiscount(DiscountFacade.modelToDtoResponse(model.getDiscount()));
+        return dto;
+    }
+
+    public static List<ClientDtoResponse> modelListToDtoResponseList(List<Client> modelList) {
+        List<ClientDtoResponse> dtoList = new ArrayList<>();
+        for (Client model : modelList)
+            dtoList.add(modelToDtoResponse(model));
+        return dtoList;
+    }
+
+    public static Client createDtoRequestToModel(ClientCreateDtoRequest dto) {
+        Client model = new Client();
+        if (dto.getName() != null)
+            model.setName(dto.getName());
+        if (dto.getSurname() != null)
+            model.setSurname(dto.getSurname());
+        if (dto.getPhoneNumber() != null)
+            model.setPhoneNumber(dto.getPhoneNumber());
+        return model;
+    }
+
+    public static Client updateDtoRequestToModel(Client model, ClientUpdateDtoRequest dto) {
+        if (dto.getName() != null)
+            model.setName(dto.getName());
+        if (dto.getSurname() != null)
+            model.setSurname(dto.getSurname());
+        if (dto.getPhoneNumber() != null)
+            model.setPhoneNumber(dto.getPhoneNumber());
+        return model;
+    }
+
+}
---
+++ src/main/java/com/example/spocktesting/util/facade/DiscountFacade.java
@@ -0,0 +1,46 @@
+package com.example.spocktesting.util.facade;
+
+import com.example.spocktesting.dto.request.discount.DiscountCreateDtoRequest;
+import com.example.spocktesting.dto.request.discount.DiscountUpdateDtoRequest;
+import com.example.spocktesting.dto.response.discount.DiscountDtoResponse;
+import com.example.spocktesting.model.Discount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscountFacade {
+
+    public static DiscountDtoResponse modelToDtoResponse(Discount model) {
+        DiscountDtoResponse dto = new DiscountDtoResponse();
+        if (model.getId() != null)
+            dto.setId(model.getId());
+        if (model.getDiscountName() != null)
+            dto.setDiscountName(model.getDiscountName());
+        dto.setPercentage(model.getPercentage());
+        return dto;
+    }
+
+    public static List<DiscountDtoResponse> modelListToDtoResponseList(List<Discount> modelList){
+        List<DiscountDtoResponse> dtoList = new ArrayList<>();
+        for(Discount model : modelList) {
+            dtoList.add(modelToDtoResponse(model));
+        }
+        return dtoList;
+    }
+
+    public static Discount createDtoRequestToModel(DiscountCreateDtoRequest dto) {
+        Discount model = new Discount();
+        if (dto.getDiscountName() != null)
+            model.setDiscountName(dto.getDiscountName());
+        model.setPercentage(dto.getPercentage());
+        return model;
+    }
+
+    public static Discount updateDtoRequestToModel(Discount model, DiscountUpdateDtoRequest dto) {
+        if (dto.getDiscountName() != null)
+            model.setDiscountName(dto.getDiscountName());
+        model.setPercentage(dto.getPercentage());
+        return model;
+    }
+
+}
