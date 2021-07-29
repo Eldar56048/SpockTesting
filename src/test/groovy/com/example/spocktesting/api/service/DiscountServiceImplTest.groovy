39ea6d87713894095ea02a2d74da93d089469f54
IBNSINA <2002>
2021-07-29 14:39:47 +0600

    Update DiscountServiceImplTest.groovy

--- src/test/groovy/com/example/spocktesting/api/service/DiscountServiceImplTest.groovy
+++ src/test/groovy/com/example/spocktesting/api/service/DiscountServiceImplTest.groovy
@@ -5,11 +5,11 @@ import com.example.spocktesting.dto.request.discount.DiscountUpdateDtoRequest
 import com.example.spocktesting.model.Discount
 import com.example.spocktesting.repository.DiscountRepository
 import com.example.spocktesting.service.implementation.DiscountServiceImpl
-import org.mockito.InjectMocks
 import spock.lang.Specification
 import spock.lang.Subject
 
 class DiscountServiceImplTest extends Specification{
+
     @Subject
     DiscountServiceImpl service
     DiscountRepository repository = Mock(DiscountRepository)
@@ -18,16 +18,15 @@ class DiscountServiceImplTest extends Specification{
         service = new DiscountServiceImpl(repository)
     }
 
-
-
-
     def "method getAll should return list of discounts"() {
         given:
-            1 * repository.findAll() >> createListOfDiscount()
-        and:
             def expected = createListOfDiscount()
+        and:
+            repository.findAll() >> createListOfDiscount()
+
         when:
             def actual = service.getAll()
+
         then:
             expected == actual
     }
@@ -35,16 +34,13 @@ class DiscountServiceImplTest extends Specification{
     def "create discount method test"() {
         given:
             def createDto = new DiscountCreateDtoRequest("VIP", 50)
-        and:
             def expected = new Discount(1, "VIP", 50)
         and:
-            1 * repository.save(_ as Discount) >> expected
-        and:
-            1 * repository.existsByDiscountName("VIP")
-        and:
-            1 * repository.existsByPercentage(50)
+            repository.save(_ as Discount) >> expected
+
         when:
             def actual = service.create(createDto)
+
         then:
             actual.id != null
             actual.discountName == createDto.discountName
@@ -54,18 +50,14 @@ class DiscountServiceImplTest extends Specification{
     def "update Discount method test"() {
         given:
             def updateDto = new DiscountUpdateDtoRequest(1, "VIP NEW", 60)
-        and:
             def expected = new Discount(1, "VIP NEW", 60)
         and:
-            1 * repository.existsByDiscountNameAndIdNotLike(updateDto.discountName, updateDto.id)
-        and:
-            1 * repository.existsByPercentageAndIdNotLike(updateDto.percentage, updateDto.id)
-        and:
-            1 * repository.getById(1) >> new Discount(1, "VIP", 50)
-        and:
-            1 * repository.save(_ as Discount) >> { Discount discount -> discount }
+            repository.getById(1) >> new Discount(1, "VIP", 50)
+            repository.save(_ as Discount) >> { Discount discount -> discount }
+
         when:
             def actual = service.update(updateDto)
+
         then:
             expected == actual
     }
@@ -73,12 +65,13 @@ class DiscountServiceImplTest extends Specification{
     def "get Discount method test"() {
         given:
             def id = 1
-        and:
             def expected = new Discount(1, "VIP", 50)
         and:
-            1 * repository.getById(id) >> expected
+            repository.getById(id) >> expected
+
         when:
             def actual = service.get(id)
+
         then:
             expected == actual
     }
@@ -86,8 +79,10 @@ class DiscountServiceImplTest extends Specification{
     def "delete Discount test"() {
         given:
             def id = 1
+
         when:
             service.delete(id)
+
         then:
             1 * repository.deleteById(id)
     }
@@ -96,15 +91,14 @@ class DiscountServiceImplTest extends Specification{
         given:
             def id = 1
         and:
-        and:
-            1 * repository.existsById(id) >> true
+            repository.existsById(id) >> true
+
         when:
             def actual = service.existsById(id)
+
         then:
             actual
     }
-
-    
 
     def createListOfDiscount() {
         def discounts = new ArrayList()
